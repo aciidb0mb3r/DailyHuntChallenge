99fbb77c03e711e3d34e15273a91ced28a01fc65
Ankit Aggarwal <ankit.spd@gmail.com>
2015-10-31 13:46:10 +0530

    Add Project

---
+++ DailyHunt/DailyHunt/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  DailyHunt
+//
+//  Created by Ankit Aggarwal on 31/10/15.
+//  Copyright © 2015 Ankit. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
