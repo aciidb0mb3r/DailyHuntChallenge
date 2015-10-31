0896ad3d8ad904d25e4323fcd005bb849697218a
Ankit Aggarwal <ankit.spd@gmail.com>
2015-10-31 15:37:58 +0530

    Add search and pull to refresh to home vc

--- DailyHunt/DailyHunt/Controllers/HomeViewController.m
+++ DailyHunt/DailyHunt/Controllers/HomeViewController.m
@@ -12,10 +12,12 @@
 #import "NewsFeedManager.h"
 #import <UIImageView+WebCache.h>
 
-@interface HomeViewController () <UITableViewDataSource, UITableViewDelegate>
+@interface HomeViewController () <UITableViewDataSource, UITableViewDelegate, UISearchResultsUpdating, UISearchBarDelegate>
 
+@property (strong, nonatomic) UISearchController *searchController;
 @property (nonatomic, weak) IBOutlet UITableView *tableView;
 @property (nonatomic, strong) NSArray<NewsArticle *> *articles;
+@property (nonatomic, strong) NSArray<NewsArticle *> *fetchedArticles;
 
 @end
 
@@ -23,15 +25,44 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    
+    [self searchSetup];
+    [self beginNewsFetch];
+    [self setupPullToRefresh];
+}
+
+- (void)searchSetup {
+    self.searchController = [[UISearchController alloc] initWithSearchResultsController:nil];
+    self.searchController.searchResultsUpdater = self;
+    self.searchController.dimsBackgroundDuringPresentation = NO;
+    self.searchController.searchBar.delegate = self;
+    self.tableView.tableHeaderView = self.searchController.searchBar;
+    self.definesPresentationContext = YES;
+    [self.searchController.searchBar sizeToFit];
+}
+
+- (void)beginNewsFetch {
     NewsFeedManager *newsFeedManager = [NewsFeedManager sharedInstance];
     [newsFeedManager fetchNewsCompletion:^(NSArray<NewsArticle *> *results, NSError *error) {
         if(error) {
             [self showAlertForError:error];
             return;
         }
+        self.fetchedArticles = results;
         self.articles = results;
         [self.tableView reloadData];
     }];
+}
+
+- (void)setupPullToRefresh {
+    UIRefreshControl *refreshControl = [[UIRefreshControl alloc] init];
+    [refreshControl addTarget:self action:@selector(refreshResults:) forControlEvents:UIControlEventValueChanged];
+    [self.tableView addSubview:refreshControl];
+}
+
+- (void)refreshResults:(UIRefreshControl *)refreshControl {
+    [self beginNewsFetch];
+    [refreshControl endRefreshing];
 }
 
 - (void)showAlertForError:(NSError *)error {
@@ -39,6 +70,26 @@
     UIAlertAction *okAction = [UIAlertAction actionWithTitle:@"Ok" style:UIAlertActionStyleCancel handler:nil];
     [alertController addAction:okAction];
     [self presentViewController:alertController animated:YES completion:nil];
+}
+
+#pragma mark - UISearchBarDelegate
+
+- (void)searchBarCancelButtonClicked:(UISearchBar *)searchBar {
+    self.articles = self.fetchedArticles;
+    [self.tableView reloadData];
+}
+
+#pragma mark - UISearchResultsUpdating
+
+- (void)updateSearchResultsForSearchController:(UISearchController *)searchController {
+    NSString *searchString = searchController.searchBar.text;
+    if(searchString.length > 0) {
+        NSPredicate *predicate = [NSPredicate predicateWithFormat:@"self.title contains[c] %@ OR self.source contains[c] %@", searchString, searchString];
+        self.articles = [self.fetchedArticles filteredArrayUsingPredicate:predicate];
+    } else {
+        self.articles = self.fetchedArticles;
+    }
+    [self.tableView reloadData];
 }
 
 #pragma mark - UITableViewDataSource
